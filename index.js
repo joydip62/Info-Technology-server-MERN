3042ac76c809768f59982bb3549398563f21cd19
JPS Archives <joydipbanik6@gmail.com>
2023-10-19 13:08:05 +0530

    add to cart product insert into database

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
-
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-
 const port = process.env.PORT || 5000;
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gufheyv.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,23 +24,40 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      
+    await client.connect();
 
-      const productCollection = client.db('technologyDB').collection('products');
+    const productCollection = client.db("technologyDB").collection("products");
 
+    const productCartCollection = client
+      .db("technologyDB")
+      .collection("productCart");
 
-      //   get all product
-      app.get('/products', async (req, res) => {
-          const result = await productCollection.find().toArray();
-          res.send(result);
-      })
-      //   insert product data
-      app.post("/products", async (req, res) => {
-          const products = req.body;
-          const result = await productCollection.insertOne(products)
-        res.send(result);
-      });
+    //   get all product
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+    //   insert product data
+    app.post("/products", async (req, res) => {
+      const products = req.body;
+      const result = await productCollection.insertOne(products);
+      res.send(result);
+    });
+
+    // single product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    //   insert product data into cart
+    app.post("/product/cart", async (req, res) => {
+      const cartProducts = req.body;
+      const result = await productCartCollection.insertOne(cartProducts);
+      res.send(result);
+    });
 
 
 
@@ -60,8 +73,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-    console.log(`App running port ${port}`);
-})
-
+  console.log(`App running port ${port}`);
+});
